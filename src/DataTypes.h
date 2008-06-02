90d0880a323dc9a0fa532b77216d755f2b27e231
gigix1980 <gigix1980@a62ada63-994e-0410-b895-97bbc69994db>
2008-06-02 00:41:03 +0000

    memory read/write

---
+++ src/DataTypes.h
@@ -0,0 +1,17 @@
+#ifndef _HEC_DATA_TYPES
+#define _HEC_DATA_TYPES 1
+
+#define S1          signed char
+#define S2          signed short
+#define S4          signed long
+#define S8          signed __int64
+   
+#define U1          unsigned char
+#define U2          unsigned short
+#define U4          unsigned long
+#define U8          unsigned __int64
+
+#define F4          float
+#define F8          double
+
+#endif
--- src/VM.cpp
+++ src/VM.cpp
@@ -1,12 +1,35 @@
 #include "VM.h"
 
+#include <cstring>
+
 VM::VM() {
-	VM(1024, 1024, 4096);
+	initialize(1024, 1024, 4096);
 }
 
 VM::VM(int stack_size, int heap_size, int text_size) {
-	RAM = 0;
+	initialize(stack_size, heap_size, text_size);
+}
+
+void VM::initialize(int stack_size, int heap_size, int text_size) {
+	int memory_size = stack_size + heap_size + text_size;
+	RAM = new U1[memory_size];
+	memset(RAM, 0, memory_size * sizeof(U1));
+	
+	BS = 0;
 	BE = HS = text_size;
 	HE = SS = HS + heap_size;
 	TOP = SS + stack_size;
+}
+
+VM::~VM() {
+//	it causes an error message on Mac OS X
+//	delete[] RAM;
+}
+
+U1 VM::read(int index) {
+	return RAM[index];
+}
+
+void VM::write(int index, U1 value) {
+	RAM[index] = value;
 }
