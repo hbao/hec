bbb8f2bba77cf4ad747946dfb58c2ba702d38ca7
gigix1980 <gigix1980@a62ada63-994e-0410-b895-97bbc69994db>
2008-06-02 00:41:03 +0000

    memory read/write

--- trunk/src/VM.h
+++ trunk/src/VM.h
@@ -1,11 +1,23 @@
 #ifndef _HEC_VM
 #define _HEC_VM 1
 
+#include "DataTypes.h"
+
 class VM {
 public:
-	int RAM, BE, HS, HE, SS, TOP;
+	int BS, BE, HS, HE, SS, TOP;
+
 	VM();
 	VM(int stack_size, int heap_size, int text_size);
+	virtual ~VM();
+	
+	U1 read(int index);
+	void write(int index, U1 value);
+
+private:
+	U1 * RAM;
+	
+	void initialize(int stack_size, int heap_size, int text_size);
 };
 
 #endif
--- trunk/test/EnvironmentTestSuite.h
+++ trunk/test/EnvironmentTestSuite.h
@@ -10,10 +10,18 @@ public:
 	
 	void testInitializeMemoryLayout(void) {
 		VM vm(1024, 1024, 2048);
-		TS_ASSERT_EQUALS(4096, vm.TOP - vm.RAM);
-		TS_ASSERT_EQUALS(2048, vm.BE - vm.RAM);
+		TS_ASSERT_EQUALS(0, vm.BS);
+		TS_ASSERT_EQUALS(4096, vm.TOP - vm.BS);
+		TS_ASSERT_EQUALS(2048, vm.BE - vm.BS);
 		TS_ASSERT_EQUALS(vm.BE, vm.HS);
 		TS_ASSERT_EQUALS(1024, vm.HE - vm.HS);
 		TS_ASSERT_EQUALS(vm.HE, vm.SS);
+	}
+	
+	void testReadAndWriteMemory(void) {
+		VM vm;
+		TS_ASSERT_EQUALS(0, vm.read(vm.BS));
+		vm.write(vm.BS, 1);
+		TS_ASSERT_EQUALS(1, vm.read(vm.BS));
 	}
 };
