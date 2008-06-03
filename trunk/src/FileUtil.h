0e5968124e4658b4dcfa8e47ba993dd2b6f39157
gigix1980 <gigix1980@a62ada63-994e-0410-b895-97bbc69994db>
2008-06-03 13:53:42 +0000

    handle magic number in HEC file and list a minimal set of instructions

--- trunk/src/FileUtil.h
+++ trunk/src/FileUtil.h
@@ -5,6 +5,12 @@
 
 class FileUtil {
 public:
+	U1 * loadHECFile(const char * file_path) {
+		int file_length = getFileLength(file_path);
+		return getFileContent(file_path, file_length);
+	};
+	
+private:
 	int getFileLength(const char * file_path) {
 		ifstream hec_file(file_path, ios::in|ios::binary);
 		long start = hec_file.tellg();
@@ -17,6 +23,20 @@ public:
 	
 	U1 * getFileContent(const char * file_path, int size) {
 		ifstream hec_file(file_path, ios::in|ios::binary);
+		
+		U1 header[2];
+		header[0] = hec_file.get();
+		header[1] = hec_file.get();
+		U2 magic_number = 0xDEED;
+		if(!memcmp(header, (char*)&magic_number, 2)) {
+			printf("\nValidation ERROR -- %s\n", file_path);
+			throw "Invalid HEC File";
+		}
+		
+		return loadInstructionSection(hec_file, size - 2);
+	}
+	
+	U1 * loadInstructionSection(ifstream & hec_file, int size) {
 		U1 * bytes = new U1[size + 1];
 		for(int i = 0; i < size; i++) {
 			bytes[i] = hec_file.get();
